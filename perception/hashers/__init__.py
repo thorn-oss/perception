6e40f6c6f0a2b2ac57cc8adc9964cdce0a041612
Fausto Morales <faustomorales@gmail.com>
2020-07-15 22:06:25 -0500

    Change default imports to avoid dependency extensions.

--- perception/hashers/__init__.py
+++ perception/hashers/__init__.py
@@ -3,7 +3,6 @@ from .image.average import AverageHash
 from .image.phash import PHash, PHashU8, PHashF
 from .image.wavelet import WaveletHash
 from .image.opencv import MarrHildreth, BlockMean, ColorMoment
-from .image.pdq import PDQHash
 from .image.dhash import DHash
 from .video.framewise import FramewiseHasher
 from .video.scenes import SimpleSceneDetection
@@ -11,6 +10,6 @@ from .video.tmk import TMKL1, TMKL2
 
 __all__ = [
     'AverageHash', 'PHash', 'WaveletHash', 'MarrHildreth', 'BlockMean',
-    'ColorMoment', 'PDQHash', 'DHash', 'FramewiseHasher', 'TMKL1', 'TMKL2',
+    'ColorMoment', 'DHash', 'FramewiseHasher', 'TMKL1', 'TMKL2',
     'PHashU8', 'PHashF', 'SimpleSceneDetection'
 ]
--- perception/hashers/image/__init__.py
+++ perception/hashers/image/__init__.py
@@ -2,10 +2,9 @@ from .average import AverageHash
 from .phash import PHash, PHashF, PHashU8
 from .wavelet import WaveletHash
 from .opencv import MarrHildreth, BlockMean, ColorMoment
-from .pdq import PDQHash, PDQHashF
 from .dhash import DHash
 
 __all__ = [
     'AverageHash', 'PHash', 'WaveletHash', 'MarrHildreth', 'BlockMean',
-    'ColorMoment', 'PDQHash', 'DHash', 'PHashF', 'PDQHashF', 'PHashU8'
+    'ColorMoment', 'DHash', 'PHashF', 'PHashU8'
 ]
