c950ae8311a9b3eda85c47aeb281c99f03b40611
Michael Simpson <michael@wearethorn.org>
2022-04-11 16:55:30 -0400

    Update build scripts

--- build-scripts/linux.sh
+++ build-scripts/linux.sh
@@ -2,7 +2,7 @@
 set -e -x
 
 # Install a system package required by our library
-yum install -y atlas-devel
+yum install -y atlas-devel python-devel
 
 # Compile wheels
 for PYBIN in /opt/python/*/bin; do
--- build-scripts/windows.bat
+++ build-scripts/windows.bat
@@ -1,6 +1,6 @@
-call conda create -n py36 -y python=3.6
 call conda create -n py37 -y python=3.7
 call conda create -n py38 -y python=3.8
+call conda create -n py39 -y python=3.9
 call activate py36 && pip install numpy cython && python setup.py sdist
 call activate py36 && pip install numpy cython && python setup.py bdist_wheel
 call activate py37 && pip install numpy cython && python setup.py bdist_wheel
