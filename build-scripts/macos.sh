12f15114b02d981873edb592d4830f293f8af02b
Michael Simpson <michael@wearethorn.org>
2022-10-03 16:03:06 -0400

    Set C++ compiler to enable successful extension build. (#92)
    Co-authored-by: Fausto Morales <fausto@robinbay.com>

--- build-scripts/macos.sh
+++ build-scripts/macos.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 HOMEBREW_NO_AUTO_UPDATE=1 brew install llvm libomp
 export CC=/usr/local/opt/llvm/bin/clang
+export CXX=/usr/local/opt/llvm/bin/clang++
 export CFLAGS="$CFLAGS -I/usr/local/opt/libomp/include"
 export LDFLAGS="$LDFLAGS -Wl,-rpath,/usr/local/opt/libomp/lib -L/usr/local/opt/libomp/lib -lomp"
 conda create -n py37 -y python=3.7
